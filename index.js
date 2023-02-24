8fddc356d5212f638bef96f7cf5957aff2d60915
dorsafbenaissa <dorsafbenaissa07@gmail.com>
2023-02-25 00:07:25 +0100

    Dorsaf

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use('/etudiant', etudiantRouter)
 app.use('/user', userRouter)
 const port = 5000
 console.log('Git')
+console.log('git dorsaf')
 mongoose
   .connect('mongodb://127.0.0.1:27017/PFE-Chaima-Dorsaf')
   .then(() => {})
