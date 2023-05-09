6450bd3ed614788cfa8ab919c9377febb08e36dd
Khaled <khaledsahli36@gmail.com>
2023-05-09 05:19:19 +0100

    Quiz

--- routes/quiz.js
+++ routes/quiz.js
@@ -18,7 +18,6 @@ router.get('/:matiere', async (req, res) => {
     }
     var questions = []
     array.map((value) => {
-      console.log(value, jsonData[value - 1])
       questions.push({
         id: jsonData[value - 1].id,
         question: jsonData[value - 1].question,
@@ -29,6 +28,33 @@ router.get('/:matiere', async (req, res) => {
   } catch (error) {
     res.status(400).send({ error: error.message })
   }
+})
+
+/* Validate Quiz */
+router.post('/', async (req, res) => {
+  var answers = req.body.formData
+  var allQuestions = require('../public/quiz/' + req.body.matiere + '.json')
+  var questions = []
+  var finalResult = 0
+  const percent = 100 / req.body.array.length
+  req.body.array.map((i) => {
+    questions.push(allQuestions[i - 1])
+  })
+  answers.map((q) => {
+    if (q.value == allQuestions[q.key - 1].correctAnswer) finalResult++
+  })
+  var exam = new Quiz()
+  exam.type = req.body.matiere
+  exam.questions = questions
+  exam.answers = answers
+  exam.result = finalResult * percent
+  exam.user = req.body.user
+  exam.cours = req.body.cours
+  await exam.save(async (e, savedExam) => {
+    if (e != null) {
+      res.send(e)
+    } else res.send({ exam: savedExam })
+  })
 })
 
 module.exports = router
